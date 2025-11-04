0ff4b873da88f14f27370882afde794f4bcf4c4d
bharat sahu <bharatsahu0037@gmail.com>
2025-11-05 00:21:24 +0530

    install storybook

--- AnnapurnaFood/Client/src/App.jsx
+++ AnnapurnaFood/Client/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Login from "./pages/loginpage/loginpage";
 
 function App() {
-  
-
   return (
     <>
-      
+      <Login />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
